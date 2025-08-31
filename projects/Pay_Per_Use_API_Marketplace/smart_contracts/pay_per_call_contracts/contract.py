42f4781e85449c41fbb485cd6154ff964c59ffa1
Ratna Reethika Arasavilli <reethika.22.arasavilli@gmail.com>
2025-08-31 22:46:46 +0530

    Update contract.py

--- projects/Pay_Per_Use_API_Marketplace/smart_contracts/pay_per_call_contracts/contract.py
+++ projects/Pay_Per_Use_API_Marketplace/smart_contracts/pay_per_call_contracts/contract.py
@@ -2,6 +2,7 @@ from algopy import ARC4Contract, String
 from algopy.arc4 import abimethod
 
 
+
 class PayPerCallContracts(ARC4Contract):
     @abimethod()
     def hello(self, name: String) -> String:
